c0cdb50182be02b935d67538e953a18c5933e6ee
Camilo1909 <kmilo0919@gmail.com>
2025-10-08 21:04:08 -0500

    fix: Update cliente ok and include static files

--- modules/base/views.py
+++ modules/base/views.py
@@ -27,30 +27,17 @@ def client_update(request, client_id):
         client = None
         messages.error(request, "Client not found.")
         return render(request, "client/client_list.html")
-    
     if request.method == "POST":
         form = ClientForm(request.POST, instance=client)
         if form.is_valid():
-            change = False
-            if client.phone_number != form.cleaned_data['phone_number']:
-                if Client.objects.filter(phone_number=form.cleaned_data['phone_number']).exists():
-                    messages.error(request, "Phone number already exists.")
-                    return render(request, "client/client_update.html", {"client": client, "form": form})
-                else:
-                    client.phone_number = form.cleaned_data['phone_number']
-                    change = True
-            if client.name != form.cleaned_data['name']:
-                client.name = form.cleaned_data['name']
-                change = True
-            if client.last_names != form.cleaned_data['last_name']:
-                client.last_names = form.cleaned_data['last_name']
-                change = True
-            if client.email != form.cleaned_data['email']:
-                client.email = form.cleaned_data['email']
-                change = True
-            if change:
-                client.updated_by = account.username
-                client.save()
+            if Client.objects.exclude(id=client.id).filter(phone_number=form.cleaned_data['phone_number']).exists():
+                messages.error(request, "Phone number already exists.")
+                return render(request, "client/client_update.html", {"client": client, "form": form})
+            
+            client = form.save(commit=False)
+            client.updated_by = account.username
+            client.save()
+
             messages.success(request, "Client updated successfully.")
             return redirect("client_list")
     else:
